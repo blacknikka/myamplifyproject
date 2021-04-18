c02b90425c540c10518d7b63906aaed142e97d6a
Yasuhiro <yasuhiro1026a@hotmail.com>
2021-04-18 23:56:54 +0900

    first commit

---
+++ src/components/CreateBlog.vue
@@ -0,0 +1,24 @@
+<template>
+  <div @click="createBlog">
+    create blog
+  </div>
+</template>
+
+<script>
+import { DataStore } from '@aws-amplify/datastore';
+import { Blog } from '../models';
+
+export default {
+  methods: {
+    async createBlog() {
+      console.log('cerate')
+      await DataStore.save(
+        new Blog({
+          name: "Lorem ipsum dolor sit amet",
+          posts: [],
+        })
+      );
+    },
+  },
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,14 +27,20 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <create-blog></create-blog>
   </div>
 </template>
 
 <script>
+import CreateBlog from './CreateBlog.vue';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components: {
+    CreateBlog,
   }
 }
 </script>
